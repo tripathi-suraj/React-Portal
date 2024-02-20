0f8e7576885195002ce0db5ed81da3e8ff88e0ff
tripathi-suraj <surajtripathi781@gmail.com>
2024-02-20 22:20:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import PortalDemo from './PortalDemo';
+
+function App() {
+  return (
+    <>
+      <PortalDemo />
+    </>
+  );
+}
+
+export default App;
